31eb3fee078f172251ca38b8d0bcf8881490728e
DevonStephens416 <105228028+DevonStephens416@users.noreply.github.com>
2022-05-10 14:04:26 -0400

    Add files via upload

---
+++ file path script.ps1
@@ -0,0 +1,10 @@
+﻿#gets all files in a specified folder and displays info such as how many files, file size and average file size.
+#variables
+$filePath = read-host "Type file path "
+$Measurement = get-childitem -Path $filePath -Recurse -File | Measure-Object -Property Length -sum -Average
+[pscustomobject]@{
+    Count = $Measurement.Count
+    Average = $Measurement.Average
+    Sum = $Measurement.Sum
+    ComputerName = $env:Computername 
+}
