f8014e56e07562f34faaf4df58be2f2a0c455999
DevonStephens416 <105228028+DevonStephens416@users.noreply.github.com>
2022-05-10 14:11:58 -0400

    Update downloads delete script.ps1

--- downloads delete script.ps1
+++ downloads delete script.ps1
@@ -1,3 +1,5 @@
-﻿#deletes old downloads
-
-Get-ChildItem –Path C:\Users\dvnst\Downloads -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item
+﻿#deletes old files in a specified folder
+$filePath = read-host "Type the path of a folder where you want to 30+ day old items"
+#grabs user input for their desired folder
+Get-ChildItem –Path $filePath -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item
+#deletes all items that are 30 days old
