5874c2c665ef121caade895933da22de18640551
DevonStephens416 <105228028+DevonStephens416@users.noreply.github.com>
2022-05-10 14:16:22 -0400

    Update file path script.ps1

--- file path script.ps1
+++ file path script.ps1
@@ -1,6 +1,7 @@
 ﻿#gets all files in a specified folder and displays info such as how many files, file size and average file size.
 #variables
 $filePath = read-host "Type file path "
+#gets a file path from user
 $Measurement = get-childitem -Path $filePath -Recurse -File | Measure-Object -Property Length -sum -Average
 [pscustomobject]@{
     Count = $Measurement.Count
@@ -8,3 +9,4 @@ $Measurement = get-childitem -Path $filePath -Recurse -File | Measure-Object -Pr
     Sum = $Measurement.Sum
     ComputerName = $env:Computername 
 }
+#Creates a variable and stores info inside of it then uses a custom object to display all the rquested information 
